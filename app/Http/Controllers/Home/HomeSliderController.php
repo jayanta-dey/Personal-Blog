25ee58a3b17aebbf4b0b0b2b43315c3e386b408f
jayanta-dey <jayanta.dey@northsouth.edu>
2025-04-09 11:49:41 +0600

    Add home slide section to the frontend home page
    - Created a new Blade template for the home slide section.
    - Integrated dynamic content retrieval from the HomeSlide model.
    - Added responsive layout with image, title, short title, and a button.
    - Included a scroll down link and a video popup feature.

--- app/Http/Controllers/Home/HomeSliderController.php
+++ app/Http/Controllers/Home/HomeSliderController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\HomeSlide;
-//use Intervention\Image\Facades\Image;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Imagick\Driver;
 
 
 class HomeSliderController extends Controller
@@ -12,27 +13,33 @@ class HomeSliderController extends Controller
     
     public function HomeSlider()
     {   
-        $homeslide = HomeSlide::find (1);
+        $homeslide = HomeSlide::find(1);
         return view('admin.home_slide.home_slide_all',compact('homeslide'));
     } //end method HomeSlider
 
 
     public function UpdateSlider(Request $request)
-    {
+    {   
+      
         $slide_id = $request->id;
         if($request->file('home_slide')){
-
+            $manager = new ImageManager(new Driver());
             $image = $request-> file('home_slide');
-            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
-           // Image::make($image)->resize(636,852)->save('upload/home_slide_image'.$name_gen);
-            $save = 'upload/home_slide_image'.$name_gen;
-            HomeSlide :: findOrFail($slide_id)->update([
+            $name_gen = hexdec(uniqid()) .'.'. $image->getClientOriginalExtension();
+            $img = $manager ->read ($request->file('home_slide'));
+            $img = $img->resize(636,852);
+           // $image->move(public_path('upload/home_slide_image'), $name_gen);
+            $img->toJpeg(80)->save(base_path('public/upload/home_slide_image/'.$name_gen));
+            $save = 'upload/home_slide_image/' . $name_gen;
+
+            HomeSlide::findOrFail($slide_id)->update([
                 'title' => $request->title,
                 'short_title' => $request->short_title,
                 'url' => $request->url,
                 'home_slide' => $save,
             ]);
-
+            
+            
             $notification = array(
                 'message' => 'Home Slide Updated With Image Successfully',
                 'alert-type' => 'success'
@@ -54,10 +61,64 @@ class HomeSliderController extends Controller
 
         }
 
-
+        
 
 
     } //end method UpdateSlider
+   
+} //end class HomeSliderController
 
 
-}
+
+
+
+
+
+
+
+
+/*  public function UpdateSlider(Request $request)
+{   
+      
+    $slide_id = $request->id;
+    if($request->file('home_slide')){
+
+        $image = $request-> file('home_slide');
+        $name_gen = hexdec(uniqid()) .'.'. $image->getClientOriginalExtension();
+       // Image::make($image)->resize(636,852)->save('upload/home_slide_image/'.$name_gen);
+       $image->move(public_path('upload/home_slide_image'), $name_gen);
+       $save = 'upload/home_slide_image/' . $name_gen;
+
+        HomeSlide::findOrFail($slide_id)->update([
+            'title' => $request->title,
+            'short_title' => $request->short_title,
+            'url' => $request->url,
+            'home_slide' => $save,
+        ]);
+        
+        
+        $notification = array(
+            'message' => 'Home Slide Updated With Image Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+
+    }else{
+        HomeSlide :: findOrFail($slide_id)->update([
+            'title' => $request->title,
+            'short_title' => $request->short_title,
+            'url' => $request->url,
+        ]);
+
+        $notification = array(
+            'message' => 'Home Slide Updated Without Image Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+
+    }
+
+    
+
+
+}*/

ce3d7459ed38039a498ffd314defb5e08e2c9bb9
jayanta-dey <jayanta.dey@northsouth.edu>
2025-04-21 19:49:39 +0600

    Add portfolio section with tabbed navigation and initial content

---
+++ resources/views/frontend/home_slide/portfolio.blade.php
@@ -0,0 +1,35 @@
+<section class="portfolio">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-xl-6 col-lg-8">
+            <div class="section__title text-center">
+                <span class="sub-title">04 - Portfolio</span>
+                <h2 class="title">All creative work</h2>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="tab-content" id="portfolioTabContent">
+    <div class="tab-pane show active" id="graphic" role="tabpanel" aria-labelledby="graphic-tab">
+        <div class="container">
+            <div class="row gx-0 justify-content-center">
+                <div class="col">
+                    <div class="portfolio__active">
+                        <div class="portfolio__item">
+                            <div class="portfolio__thumb">
+                                <img src="{{asset('frontend/assets/img/portfolio/portfolio_img02.jpg')}}" alt="">
+                            </div>
+                            <div class="portfolio__overlay__content">
+                                <span>Web Design</span>
+                                <h4 class="title"><a href="portfolio-details.html">Graphic Design Management System</a></h4>
+                                <a href="portfolio-details.html" class="link">Case Study</a>
+                            </div>
+                        </div>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
